24dd2a06047fff9245875a049f9be8ef4921d1ca
Youichi Kato <youichikato@gmail.com>
2012-02-20 07:07:11 +0900

    Add opera to tagets for jsTestdriver

--- test_jstestdriver/script/jstestdriver.sh
+++ test_jstestdriver/script/jstestdriver.sh
@@ -8,12 +8,13 @@ JSTESTDRIVER_CONF=${CWD}/conf/jsTestDriver.conf
 SAFARI=script/safari
 FIREFOX=script/firefox
 CHROME=script/chrome
+OPERA=script/opera
 
-FULL=0
+FULL=1
 if [ $FULL = 0 ];then
     BROWSERS=${CHROME}
 else
-    BROWSERS=${SAFARI},${FIREFOX},${CHROME}
+    BROWSERS=${SAFARI},${FIREFOX},${CHROME},${OPERA}
 fi
 
 (java -jar ${JSTESTDRIVER_BIN} \
